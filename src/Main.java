0606970e385e91fa69aaf238250c4c94957c5bea
Student 3 <>
2023-08-25 12:02:27 -0400

    I made 3 trails operational out of 5, and I made loading screens for them, and used else and if's and I think that is it.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,225 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        String answer;
+        System.out.println("Hello and welcome! what is your name?");
+        String name = sc.nextLine();
+        System.out.println("are you sure you wanna choose " + name + "? (yes/no)");
+        answer = sc.nextLine();
+        if(answer.equals("no")){
+            System.out.println("What is your name?");
+            name = sc.nextLine();
+            System.out.println("Welcome " + name + "!");
         }
+
+        System.out.println("Hello and Welcome to the trail! I am your guide, which trail do you want to go on first? Trail of despair, Trail of nature, Trail of water, and the Normal trail, which one? Or do you wanna do all of them?");
+        answer = sc.nextLine();
+        if(answer.equals("Trail of despair")){
+
+                System.out.println("are you sure you wanna choose " + answer + "? (yes/no");
+                answer = sc.nextLine();
+                if(answer.equals("yes")) {
+                System.out.println("Loading Trail...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Feeding Bison's...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Wagon...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Wagon wheels...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Animals...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("loading Boots and Gear...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Weapons...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("loading Trail...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Complete!");
+                TimeUnit.SECONDS.sleep(3);
+            }
+            else {
+                    System.out.println("Which trail do you wanna go on first?");
+                    System.out.println("Do you wanna choose the Trail of nature?");
+                }
+
+                    System.out.println("are you sure you wanna choose " + answer + "? (yes/no");
+                    answer = sc.nextLine();
+                    if(answer.equals("yes")){
+                        System.out.println("Loading Trail...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Feeding Bison's...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Loading Wagon...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Loading Wagon wheels...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Loading Animals...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("loading Boots and Gear...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Loading Weapons...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("loading Trail...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Loading ...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Loading ...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Loading ...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Loading ...");
+                        TimeUnit.SECONDS.sleep(3);
+                        System.out.println("Loading Complete!");
+                        TimeUnit.SECONDS.sleep(3);
+                    }   else {
+                        System.out.println("Which trail do you wanna go on first?");
+                        System.out.println("Do you wanna choose the Trail of fire?");
+                    }
+            System.out.println("are you sure you wanna choose " + answer + "? (yes/no");
+            answer = sc.nextLine();
+            if(answer.equals("yes")) {
+                System.out.println("Loading Trail...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Feeding Bison's...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Wagon...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Wagon wheels...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Animals...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("loading Boots and Gear...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Weapons...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("loading Trail...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Complete!");
+                TimeUnit.SECONDS.sleep(3);
+            }else {
+                System.out.println("Which trail do you wanna go on first?");
+                System.out.println("Do you wanna choose the Trail of fire?");
+            }
+            System.out.println("are you sure you wanna choose " + answer + "? (yes/no");
+            answer = sc.nextLine();
+            if(answer.equals("yes")) {
+                System.out.println("Loading Trail...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Feeding Bison's...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Wagon...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Wagon wheels...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Animals...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("loading Boots and Gear...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Weapons...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("loading Trail...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Complete!");
+                TimeUnit.SECONDS.sleep(3);
+            }else {
+                System.out.println("Which trail do you wanna go on first?");
+                System.out.println("Do you wanna choose the normal trail?");
+
+        }
+            System.out.println("are you sure you wanna choose " + answer + "? (yes/no");
+            answer = sc.nextLine();
+            if(answer.equals("yes")) {
+                System.out.println("Loading Trail...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Feeding Bison's...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Wagon...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Wagon wheels...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Animals...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("loading Boots and Gear...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Weapons...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("loading Trail...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading ...");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Loading Complete!");
+                TimeUnit.SECONDS.sleep(3);
+            }else{
+                System.out.println("since you don't want to do any of them... I guess you can leave :( or do you wanna do all of the trails?");
+                System.out.println("are you sure you wanna choose " + answer + "? (yes/no");
+                answer = sc.nextLine();
+                if(answer.equals("yes")) {
+                    System.out.println("Loading Trail...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Feeding Bison's...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Loading Wagon...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Loading Wagon wheels...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Loading Animals...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("loading Boots and Gear...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Loading Weapons...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("loading Trail...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Loading ...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Loading ...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Loading ...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Loading ...");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("Loading Complete!");
+                    TimeUnit.SECONDS.sleep(3);
+
+
+
+
     }
-}
+}}}}
